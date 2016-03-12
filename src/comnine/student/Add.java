62b3d995b4002901e28edd42f16921c632ecc7c5
Ninse <245328522@qq.com>
2016-03-12 12:36:27 +0800

    学生成绩管理系统V1.0

---
+++ src/comnine/student/Add.java
@@ -0,0 +1,16 @@
+package comnine.student;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Add extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add);
+
+	}
+
+}
